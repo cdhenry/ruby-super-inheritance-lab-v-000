3bc8d74f5c938dd2320004790db5829df1a23df5
Chris Henry <cdhenry@gmail.com>
2018-02-17 19:13:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,9 @@
 class Student
   def hello
-    return "Hey there! I'm so excited to learn stuff.\n"
+    puts "Hey there! I'm so excited to learn stuff."
   end
 
   def raise_hand
-    return "Pick me!\n"
+    puts "Pick me!"
   end
 end
